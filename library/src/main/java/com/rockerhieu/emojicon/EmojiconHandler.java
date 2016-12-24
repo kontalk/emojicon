e83aa8c05c5038fec003025e12d5f3e8c1ef3757
Daniele Ricci <daniele.athome@gmail.com>
2016-12-24 16:55:41 +0100

    modify emojiconhandler to check if follow unicode is a valid emoji modifier before looking it up to prevent useless lookups on normal text, should address #106 #111
    Signed-off-by: Daniele Ricci <daniele.athome@gmail.com>

--- library/src/main/java/com/rockerhieu/emojicon/EmojiconHandler.java
+++ library/src/main/java/com/rockerhieu/emojicon/EmojiconHandler.java
@@ -31,7 +31,16 @@ public final class EmojiconHandler {
 
     private static final SparseIntArray sEmojisMap = new SparseIntArray(1029);
     private static final SparseIntArray sSoftbanksMap = new SparseIntArray(471);
+    private static final SparseIntArray sEmojiModifiersMap = new SparseIntArray(5);
     private static Map<String, Integer> sEmojisModifiedMap = new HashMap<>();
+    
+    static {
+        sEmojiModifiersMap.put(0x1f3fb, 1);
+        sEmojiModifiersMap.put(0x1f3fc, 1);
+        sEmojiModifiersMap.put(0x1f3fd, 1);
+        sEmojiModifiersMap.put(0x1f3fe, 1);
+        sEmojiModifiersMap.put(0x1f3ff, 1);
+    }
 
     static {
         // People
@@ -1684,9 +1693,10 @@ public final class EmojiconHandler {
                         }
                         skip += followSkip;
 
-                    } else {
+                    } else if(sEmojiModifiersMap.get(followUnicode, 0) > 0){
                         //handle other emoji modifiers
                         int followSkip = Character.charCount(followUnicode);
+
 
                         //TODO seems like we could do this for every emoji type rather than having that giant static map, maybe this is too slow?
                         String hexUnicode = Integer.toHexString(unicode);
@@ -1710,8 +1720,10 @@ public final class EmojiconHandler {
                             icon = resourceId;
                         }
                         skip += followSkip;
-                    }
-                }
+                    }else{
+                        int followSkip = Character.charCount(followUnicode);
+                        skip += followSkip;
+                    }                }
             }
 
             if (icon > 0) {
